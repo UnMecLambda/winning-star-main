97a1e10731cf6c79a11396b8b7325586f8e8f3d5
UnMecLambda <53877761+UnMecLambda@users.noreply.github.com>
2025-08-24 21:27:52 +0200

    Updated home.component.ts

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -6,6 +6,7 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+export class HomeComponent implements OnInit, OnDestroy {
   isAuthenticated = false;
   onlinePlayers = 1247;
   platformStats = {
